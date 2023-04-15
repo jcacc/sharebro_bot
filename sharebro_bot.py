651dfe5000e81563965b2c077087244740a45404
jcacc <joe@accardi.xyz>
2023-04-15 10:02:40 -0400

    preparing for deployment to shell

--- sharebro_bot.py
+++ sharebro_bot.py
@@ -1,11 +1,15 @@
 import os
 import discord
+import random
 from discord.ext import commands
 from dotenv import load_dotenv
 
 load_dotenv()
 
 TOKEN = os.getenv('TOKEN')
+print(TOKEN)
+
+
 
 intents = discord.Intents.all()
 intents.members = True
@@ -18,6 +22,9 @@ bot = commands.Bot(command_prefix='', intents=intents)
 async def on_ready():
     print(f'Logged in as {bot.user.name}')
 
+# # Setting `Listening` status
+#     await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name="a really annoying song on repeat."))
+
 @bot.event
 async def on_message(message):
     if message.author == bot.user:
@@ -28,4 +35,22 @@ async def on_message(message):
 
     await bot.process_commands(message)
 
+@bot.event
+async def on_message(message):
+    if message.content.startswith('!roll'):
+        roll = message.content.split(' ')[1]
+        try:
+            rolls, limit = map(int, roll.split('d'))
+            if rolls > 25:
+                await message.channel.send('I can only roll up to 25 dice at once.')
+                return
+        except Exception:
+            await message.channel.send('Format has to be in NdN!')
+            return
+
+        result = ', '.join(str(random.randint(1, limit)) for r in range(rolls))
+        await message.channel.send(result)
+
+
 bot.run(TOKEN)
+
